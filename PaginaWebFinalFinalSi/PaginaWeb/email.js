2abb23ea45288399e5b7c7547a82cdb4977d654d
TheChosenOne7373836 <tadeods2005@gmail.com>
2023-04-11 17:47:48 -0300

    Add

--- PaginaWebFinalFinalSi/PaginaWeb/email.js
+++ PaginaWebFinalFinalSi/PaginaWeb/email.js
@@ -1,7 +1,23 @@
 function add() {
-    var a = document.getElementById('a').value;
-    var b = document.getElementById('b').value;
+    textObject.required = true|false;
+
+    var usuario = document.getElementById('userName').required = true;
+    var password = document.getElementById('userPassword').required = true;
+    var identification = document.getElementById('userIdent').required = true;
+    var genero = document.getElementById('userGenre').required = true;
+    var edad = document.getElementById('userAge').required = true;
+    var mail = document.getElementById('userMail').required = true;
 
     var sum = parseInt(a) + parseInt(b);
     alert(sum);
+
+    const submitButton = document.querySelector('.btn');
+    submitButton.addEventListener("click", function(event) {
+        console.log("Testing");
+    })
+
+    if(usuario = null){
+        alert 
+    }
+    
 }
