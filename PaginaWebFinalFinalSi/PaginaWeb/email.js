656de2584b7bbd8ecfddf4facf5534fe49b6978b
TheChosenOne7373836 <tadeods2005@gmail.com>
2023-04-25 17:46:09 -0300

    Forms JQuery WIP

--- PaginaWebFinalFinalSi/PaginaWeb/email.js
+++ PaginaWebFinalFinalSi/PaginaWeb/email.js
@@ -1,11 +1,47 @@
-var formulario = document.getElementById("FormsUser");
-var enviar = document.getElementById("enviarForms");
+//JS
 
-enviar.addEventListener("click", function(event) {
+//var formulario = document.getElementById("FormsUser");
+//var enviar = document.getElementById("enviarForms");
 
-  if (!formulario.checkValidity()) {
+//enviar.addEventListener("click", function(event) {
 
-    alert("Por favor completar los campos");
-    event.preventDefault(); 
-  }
-});
+//  if (!formulario.checkValidity()) {
+
+//    alert("Por favor completar los campos");
+//    event.preventDefault(); 
+//  }
+//});
+
+
+//JQuery
+
+$(document).ready(function(){
+  $("#enviarForms").click(function(){
+
+    var nombre = $("#ITnombre").val();
+    var contrasena = $("#ITpassword").val(); 
+    var mail = $("#ITmail").val();
+
+    if(nombre =""){
+      $("mensaje1").fadeIn();
+      return false;
+    } else {
+      $("#mensaje1").fadeOut();
+    }
+
+    if(contrasena =""){
+      $("mensaje2").fadeIn();
+      return false;
+    } else {
+      $("#mensaje2").fadeOut();
+    }
+
+    if(mail =""){
+      $("mensaje3").fadeIn();
+      return false;
+    } else {
+      $("#mensaje3").fadeOut();
+    }
+
+  });
+})
