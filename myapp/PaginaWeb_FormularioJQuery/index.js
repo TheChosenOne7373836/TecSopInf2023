9876dafe0de939f75febc9358089af642b5d9870
TheChosenOne7373836 <tadeods2005@gmail.com>
2023-06-27 11:33:40 -0300

    Node Done

--- myapp/PaginaWeb_FormularioJQuery/index.js
+++ myapp/PaginaWeb_FormularioJQuery/index.js
@@ -1,73 +1,38 @@
-const express = require('express');
-const mysql = require('mysql');
+$(document).ready(function() {
+    $('#FormsUser').submit(function(event) {
 
-const app = express();
-const port = 3000;
+      event.preventDefault(); // no se puede enviar form. como està
 
-// Configurar la conexión a la base de datos
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'alumno',
-  password: 'alumnoipm',
-  database: 'DBTesting'
-});
+  
+      var nombreUsuario = $('#nombreUsuario').val().trim();
+      var passwordUsuario = $('#passwordUsuario').val().trim();
+      var mailUsuario = $('#mailUsuario').val().trim();
+  
+      // oculta mensajes de error
+      $('.errores').hide();
+  
+      if (nombreUsuario === '') {
+        mostrarError('#mensaje1', 'Ingrese nombre');
+      } else if (passwordUsuario === '') {
+        mostrarError('#mensaje2', 'Ingrese una contraseña');
+      } else if (mailUsuario === '') {
+        mostrarError('#mensaje3', 'Ingrese un mail');
+      } else if (!validarEmail(mailUsuario)) {
+        mostrarError('#mensaje3', 'Mail invalido');
+      } else {
+        // redirecciona a archivo en misma carpeta
 
-connection.connect((error) => {
-  if (error) {
-    console.error('Error al conectar a DB: ' + error.stack);
-    return;
-  }
-  console.log('Conectado a DB');
-});
-
-
-// Manejar la solicitud POST del formulario
-  const nombreUsuario = $('#nombreUsuario').val().trim();
-  const passwordUsuario = $('#passwordUsuario').val().trim();
-  const mailUsuario = $('#mailUsuario').val().trim();
-
-  if (nombreUsuario === '') {
-    mostrarError('#mensaje1', 'Ingrese nombre');
-  } else if (passwordUsuario === '') {
-    mostrarError('#mensaje2', 'Ingrese una contraseña');
-  } else if (mailUsuario === '') {
-    mostrarError('#mensaje3', 'Ingrese un mail');
-  } else if (!validarEmail(mailUsuario)) {
-    mostrarError('#mensaje3', 'Mail invalido');
-  } else {
-    const data = {
-      name: nombreUsuario,
-      password: passwordUsuario,
-      mail: mailUsuario
-    };
-    
-    const query = 'INSERT INTO Clientes SET ?';
-    connection.query(query, data, (error, results) => {
-      if (error) {
-        console.error('Error al insertar: ' + error.stack);
-        return;
+        window.location.href = 'ThankYou.html';
       }
-      console.log('Datos insertados');
     });
-    connection.end((error) => {
-      if (error) {
-        console.error('Error al cerrar la conexion: ' + error.stack);
-        return;
-      }
-      console.log('Conexion terminada');
-    });
-  };
-
-// Iniciar el servidor
-app.listen(port, () => {
-  console.log('Puerto de servidor: ' + port);
-});
-
-function mostrarError(selector, mensaje) {
-  $(selector).text(mensaje).show();
-}
-
-function validarEmail(email) {
-  const regex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
-  return regex.test(email);
-}
+  
+    function mostrarError(selector, mensaje) {
+      $(selector).text(mensaje).show();
+    }
+  
+    function validarEmail(email) { //verifica mail
+      var regex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+      return regex.test(email);
+    }
+  });
+  
--- myapp/app.js
+++ myapp/app.js
@@ -1,16 +1,33 @@
 const express = require('express')
+const mysql = require('mysql');
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.use(express.static('./dist/client'));
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'alumno',
+  password: 'alumnoipm',
+  database: 'DBTesting'
+});
+connection.connect((error) => {
+  if (error) {
+    console.error('Error connecting to the database: ' + error.stack);
+    return;
+  }
+  console.log('Connected to the database.');
+});
 
 
 app.listen(port, () => {
-  console.log(`App totalmente funcional en puerto: [${port}]`)
-})
+  console.log(`Servidor totalmente funcional en  http://localhost:${port}`);
+});
 
 
 app.get('/', (req, res) => {
@@ -31,7 +48,101 @@ app.get('/goofyahh',(req,res) => {
 })
 
 app.get("/forms", (req, res) => {
-  res.sendFile(__dirname + '/PaginaWeb_FormularioJQuery/LogIn.html');
+  res.send(`
+  <html lang="en">
+<head class="responsive">
+    <head>
+        <title>G . SEARCH Official Website LogIn</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="/PaginaWeb_FormularioJQuery/loaderstyle.css">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+      </head>
+</head>
+<body style="background-color:#2b2c2c;">
+    <!-- NO  TOCAR  -->
+    <!-- NO  TOCAR  -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+    crossorigin="anonymous"></script>
+    <!-- NO  TOCAR  -->
+    <!-- NO  TOCAR  -->
+
+
+    <div class="responsive-page-template-content">
+      <header>
+        <nav class="navbar navbar-expand-lg bg-dark">
+
+          <div class="container-fluid">
+            <a class="navbar-brand">
+              <img src="./PagPrincipalLogo/MainLogo.png" style="width:9vw; height:4vw;">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link active text-center text-white font-monospace" aria-current="page"  style="padding-right: 4vw;">Recientemente añadido</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link text-center text-white font-monospace" >Categoria</a>
+                </li>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link text-center text-white font-monospace" >Log-In</a>
+            </li>
+            </ul>
+          </div>
+        </div>   
+      </nav>
+    </header>
+  </div>
+
+  <div class="text-center" id="container">
+          <br>
+          <form action = "/insert" id="FormsUser" style="color: white;">
+
+              <label>Nombre de Usuario:</label>
+              <input type="text" id="nombreUsuario" name="nombre" class="campo" placeholder = "Nombre">
+              <div id="mensaje1" class="errores"></div>
+              <br><br><br><br>
+
+              <label>Contraseña:</label>
+              <input type="text" id="passwordUsuario" name="apellido" class="campo" placeholder = "Password">
+              <div id="mensaje2" class="errores"></div>
+              <br><br><br><br>
+
+              <label>Correo electrónico:</label>
+              <input type="text" id="mailUsuario" name="correo" class="campo" placeholder = "Mail">
+              <div id="mensaje3" class="errores"></div>
+              <br><br><br><br>
+
+              <button type="submit" id="enviarForms" class="boton">Enviar</button>
+          </form>
+      </div>
+</body>
+</html>`);
+});
+
+app.post('/insert', (req, res) => {
+  console.log(req);
+  const { Nombre, Password, Mail } = req.body;
+
+  const query = 'INSERT INTO Clientes (nombre, contrasena, correo) VALUES (?, ?, ?);';
+  connection.query(query, [Nombre, Password, Mail], (error) => {
+    if (error) {
+      console.error('error al insertar: ', error);
+      res.send('error al insertar');
+      return;
+    }
+    res.send('fila insertada correctamente');
+  });
 });
 
 app.use('/redirect', function(req, res){
@@ -49,5 +160,7 @@ app.use((req, res) =>{
   res.status(404).send('Yo soy la unica cosa que te detiene de romper el programa. Agradeceme.')
 
 })
+
+
 
 
